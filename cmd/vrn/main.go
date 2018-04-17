18521f5e6830aa9498754bb1001dbe5be07f0251
henkman <henkman@github.com>
2018-04-17 23:47:38 +0200

    added query util

---
+++ cmd/vrn/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/henkman/vrn"
+)
+
+func main() {
+	var opts struct {
+		Find        string
+		Origin      string
+		Destination string
+	}
+	flag.StringVar(&opts.Find, "f", "", "find a station")
+	flag.StringVar(&opts.Origin, "o", "", "origin")
+	flag.StringVar(&opts.Destination, "d", "", "destination")
+	flag.Parse()
+	if opts.Find != "" {
+		var s vrn.Session
+		if err := s.Init(); err != nil {
+			log.Fatal(err)
+		}
+		stops, err := s.FindStop(opts.Find)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, stop := range stops {
+			raw, err := json.MarshalIndent(stop, "", "\t")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(raw))
+		}
+		return
+	}
+	if opts.Origin != "" && opts.Destination != "" {
+		var s vrn.Session
+		if err := s.Init(); err != nil {
+			log.Fatal(err)
+		}
+		trips, err := s.FindTrips(opts.Origin, opts.Destination)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, trip := range trips {
+			fmt.Printf("%s: \n", trip.Duration)
+			for _, leg := range trip.Legs {
+				var name string
+				if leg.Mode.Name != "" {
+					name = leg.Mode.Name
+				} else {
+					name = leg.Mode.Product
+				}
+				fmt.Printf("\t%s\n", name)
+				for _, point := range leg.Points {
+					var platform string
+					if point.PlatformName != "" {
+						platform = "platform " + point.PlatformName
+					}
+					fmt.Printf("\t\t%s: '%s' %s %s\n",
+						point.Usage,
+						point.Name,
+						platform,
+						point.DateTime.Time)
+				}
+			}
+		}
+		return
+	}
+	flag.Usage()
+}
